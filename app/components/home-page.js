b497566d1c210e517197250f1503f7d861a50076
applebee <applebee@applebee1558.com>
2021-06-21 14:59:39 -0700

    home page

---
+++ app/components/home-page.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+
+export default class HomePageComponent extends Component {
+    @service router
+    @action
+    goregister(){
+        this.router.transitionTo('register');
+    }
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
-
+import {make_request} from "../libs/api-handler"
 export default class IndexRoute extends Route {
+    async model(){
+        const {json_data, status} = await make_request("GET", "/stats")
+        if(status==200){
+            json_data.nice_uptime = this.secondsToDhms(json_data.uptime)
+        }
+        return json_data
+    }
+    secondsToDhms(seconds) {
+        seconds = Number(seconds);
+        var d = Math.floor(seconds / (3600*24));
+        var h = Math.floor(seconds % (3600*24) / 3600);
+        var m = Math.floor(seconds % 3600 / 60);
+        var s = Math.floor(seconds % 60);
+        
+        var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+        var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+        var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+        var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+        return dDisplay + hDisplay + mDisplay + sDisplay;
+    }
 }
